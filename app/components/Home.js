b8dbeb8a50dbbcd17b813dd48d3209b801055878
fateme <f.adiban.rad@gmail.com>
2024-11-21 22:21:09 +0330

    new

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,16 +4,29 @@ import StateContext from "../StateContext"
 
 function Home() {
   const appState = useContext(StateContext)
+  const groups = ["آمار", "اخلاق", "اقتصاد اسلامي", "تربيت  بدني", "حسابداري", "حقوق  جزا و جرم  شناسي", "حقوق  خصوصي", "حقوق بين الملل", "حقوق عمومي", "حقوق مالكيت فكري", "رياضي", "زبان  و ادبيات  انگليسي", "زبان  و ادبيات  عربي", "زبان  و ادبيات  فارسي", "زيست شناسي", "شيعه شناسي", "شيمي", "علم اطلاعات و دانش شناسي", "علوم  تربيتي", "علوم قرآن و حديث", "علوم كامپيوتر", "فقه  و مباني  حقوق  اسلامي", "فلسفه  و كلام  اسلامي", "فيزيك", "مديريت بازرگاني", "مديريت صنعتي", "معارف", "معماري", "مهندسي  صنايع", "مهندسي برق", "مهندسي شيمي", "مهندسي عمران", "مهندسي كامپيوتر", "مهندسي مكانيك"]
 
   return (
     <Page title="اساتید" wide={true}>
       <div className="row">
         <div className="col-12">
           <div className="heading-section">
-            <h3>Our Top Sell</h3>
-            <h2>Checkout Our Top Sell Burger</h2>
-            <div className="heading-borders">
-              <span className="selected"></span>
+            <div class="row height d-flex justify-content-center align-items-center">
+              <div class="col-md-6 direction">
+                <select id="group" className="form-control select-bar">
+                  {groups.map((group, index) => (
+                    <option key={index} value={group}>
+                      {group}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div class="col-md-6">
+                <div class="form direction">
+                  <i class="fa fa-search direction"></i>
+                  <input type="text" class="form-control form-input" placeholder="نام استاد را وارد کنید..." />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -36,8 +49,8 @@ function Home() {
             <h3>امیر جلالی بیدگلی</h3>
             <img className="professor-img" src="../images/profile.jpg" />
             <div className="price">
-              <p>دانشیار</p>
-              <p>گروه فیزیک</p>
+              <p className="tiny-text">دانشیار</p>
+              <p className="tiny-text">گروه فیزیک</p>
               <button className="btn btn-sm btn-success mr-2">ارسال پیام</button>
               <button className="btn btn-sm btn-success mr-2">نمایش برنامه</button>
             </div>
